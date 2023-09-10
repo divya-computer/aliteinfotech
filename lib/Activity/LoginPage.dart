5577d168c49f3654d21662ed4b4e944019effe37
Divya Shaparia <shaparia95@gmail.com>
2023-09-10 15:42:54 +0530

    LoginPage

---
+++ lib/Activity/LoginPage.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  TextEditingController tecEmail = new TextEditingController();
+  TextEditingController tecPassword = new TextEditingController();
+  String _email = "", _pass = "";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login Page'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Form(
+              key: formKey,
+              child: Column(
+                children: [
+                  TextFormField(
+                    validator: (val) =>
+                        !val!.contains("@") ? "Email Id is not Valid" : null,
+                    onSaved: (val) => _email = val!,
+                    decoration: InputDecoration(
+                        labelText: "Email", hintText: "Enter Email"),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    onSaved: (val) => _pass = val!,
+                    validator: (val) => val!.length < 6
+                        ? "Password length should be Greater than 6"
+                        : null,
+                    decoration: InputDecoration(
+                        labelText: "Password", hintText: "Enter Password"),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: ElevatedButton(
+                      child: Text("Submit"),
+                      onPressed: () {
+                        if (formKey.currentState!.validate()) {
+                          formKey.currentState!.save();
+
+                          Fluttertoast.showToast(
+                              msg: "Successfully Login",
+                              toastLength: Toast.LENGTH_SHORT,
+                              gravity: ToastGravity.BOTTOM,
+                              timeInSecForIosWeb: 1,
+                              backgroundColor: Colors.red,
+                              textColor: Colors.white,
+                              fontSize: 16.0);
+                        }
+                      },
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/Utills/Utils.dart
@@ -0,0 +1,8 @@
+class AppUrl {
+  static const String baseUrl = 'https://gorest.co.in/public/v2';
+
+  static const String getUserInfo = '$baseUrl/users';
+
+  static const String userListApi =
+      'https://webhook.site/1fc58777-2f13-4277-957f-16f463660db6';
+}
