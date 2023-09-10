0ecb3e23073d90d18435eedfc5513b9485f6c9ba
Divya Shaparia <shaparia95@gmail.com>
2023-09-10 17:31:23 +0530

    aboutus and contact us

--- lib/Activity/Aboutus.dart
+++ lib/Activity/Aboutus.dart
@@ -11,7 +11,41 @@ class _AboutUsState extends State<AboutUs> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('About Us')),
+      appBar: AppBar(
+        title: Text('About Us'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              // App or organization logo
+              Image.network(
+                'https://avatars.githubusercontent.com/u/60287575?v=4',
+                width: 150,
+                height: 150,
+              ),
+              SizedBox(height: 20),
+
+              Text(
+                'Divya Shaparia',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 10),
+
+              Text(
+                'Hello I am Flutter & Java Developer with over 2 years of Experience and i have developed more then 10 app and have a well experience and worked with several companies and Clients',
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/Activity/Contactus.dart
+++ lib/Activity/Contactus.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 
 class ContactUs extends StatefulWidget {
@@ -13,6 +15,37 @@ class _ContactUsState extends State<ContactUs> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Contact Us'),
+      ),
+      body: Column(
+        children: [
+          Center(
+            child: Text(
+              'Phone number : 9825793013',
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+          Center(
+            child: Text(
+              'Email Id : shaparia20@gmail.com',
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+          Center(
+            child: Text(
+              'github : divya-computer',
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+        ],
       ),
     );
   }
